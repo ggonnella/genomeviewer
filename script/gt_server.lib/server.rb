798ae18d64b7a41c31a0cf2e93987c98ee0415c6
Giorgio Gonnella <ggonnell@yahoo.it>
2008-12-14 18:47:29 +0100

    del comments
    those lists are going to be obsolete anyway

--- script/gt_server.lib/server.rb
+++ script/gt_server.lib/server.rb
@@ -67,41 +67,7 @@ class GTServerClass
   end
 
   include Parsing
-
-  # public methods:
-
-  # gff3_errors(filename)                 => nil or String (error msg)
-  # gff3_feature_types(filename)          => Array of strings
-  # gff3_seqids(filename)                 => Array of strings
-  # gff3_range(filename, seqid)           => Range
-  # gff3_uncache(filename)                => nil or Array of deleted objects
-  # feature_index(filename, mode)         => GT::FeatureIndex
-  # feature_index_cached?(filename, mode) => true or false
-  # feature_types_cached?(filename)       => true or false
-
   include Configuration
-
-  # public methods:
-
-  # config(key)         => GT::Style
-  # config_default      => GT::Style
-  # config_new          => GT::Style
-  # color_new           => GT::Color
-  # config_uncache(key) => nil or GT::Style
-  # config_cached?(key) => true or false
-
   include Output
-
-  # public methods:
-
-  # img_and_map_generate(uuid, filename, => true or false
-  #   seq_id, range, config_obj, width,
-  #   add_introns, config_override)
-  #
-  # img(uuid, delete=true)    => String (png image binary)
-  # map(uuid, delete=true)    => GT::ImageMap
-  # img_and_map_destroy(uuid) => nil or Array of deleted objects
-  # img_exists?(uuid)         => true or false
-  # map_exists?(uuid)         => true or false
 
 end
