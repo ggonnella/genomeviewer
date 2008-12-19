443ccccfcee3bf6c888b0177294183af40c0456d
Giorgio Gonnella <ggonnell@yahoo.it>
2008-12-19 16:40:27 +0100

    nomenclature change in helpers

--- app/helpers/feature_types_helper.rb
+++ app/helpers/feature_types_helper.rb
@@ -1,7 +1,7 @@
 module FeatureTypesHelper
 
-  def style_form_column(record, input_name)
-    opts = options_for_select(["default"]+Style::DefinedStyles.values, record.style.to_s)
+  def block_style_form_column(record, input_name)
+    opts = options_for_select(["default"]+BlockStyle::DefinedBlockStyles.values, record.block_style.to_s)
     select_tag input_name, opts
   end
 
