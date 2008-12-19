06682c31bf6c0cdede06cccf74158d7bd7b9fbe7
Giorgio Gonnella <ggonnell@yahoo.it>
2008-12-19 15:31:25 +0100

    Configuration => Style

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,44 +1,56 @@
-class Style
+class Style < ActiveRecord::Base
 
-  DefinedStyles =
-    {
-      1 => "box",
-      2 => "caret",
-      3 => "dashes",
-      4 => "line"
-    }
+  belongs_to :user
+  has_many :feature_types, :dependent => :destroy
+  has_one  :format, :dependent => :destroy
 
-  def initialize(key)
-    @key = ([1,2,3,4].include?(key.to_i) ? key.to_i : nil)
+  def section_objects
+    [format]+feature_types
   end
 
-  attr_reader :key
-
-  def string
-    DefinedStyles.fetch(key, "undefined")
+  def sections
+    section_objects.map(&:section)
   end
 
-  alias_method :to_s, :string
+  after_save       :make_sure_there_is_a_format
+  after_create     :uncache
 
-  def ==(other)
-    return nil unless other.respond_to?(:key)
-    @key == other.key
-  end
-
-  def self.undefined
-    new(nil)
-  end
-
-  def undefined?
-    @key.nil?
-  end
-
-  String.class_eval do
-
-    def to_style
-      Style.new(Style::DefinedStyles.index(self))
+  def make_sure_there_is_a_format
+    unless format(true)
+     f = Format.default_new(:style_id => self[:id])
+     f.save
+     format(true) # update the cache
     end
+  end
 
+  # reference to the GT::Style object
+  # corresponding to this object
+  #
+  # if there is no cache, upload all setting
+  # from the DB into the gt config object
+  # but allow an exception to avoid circular
+  # references
+  #
+  def gt(upload_exception_section = nil, upload_exception_attr = nil)
+    if !GTServer.style_cached?(self[:id])
+      GTServer.style(self[:id])
+      section_objects.each do |x|
+        if x.section == upload_exception_section
+          x.upload_except(upload_exception_attr)
+        else
+          x.upload
+        end
+      end
+    end
+    GTServer.style(self[:id])
+  end
+
+  def uncache
+    GTServer.style_uncache(self[:id])
+  end
+
+  def self.default
+    GTServer.style_default
   end
 
 end
