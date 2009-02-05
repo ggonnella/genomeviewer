760789ec3b6f6cb61245ee15186b0f7411e6908d
Giorgio Gonnella <ggonnell@yahoo.it>
2009-02-05 19:09:43 +0100

    viewer controller updated + functional tests

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,24 +1,25 @@
 class ViewerController < ApplicationController
 
-  before_filter :initialization, :except => "image"
+  before_filter :initialization
   
-  ### image ###
-
   def image
-    send_data GTServer.img(@annotation.gff3_data_storage, 
-                         @sequence_region.seq_id, 
-                         @start..@end,
-                         style_obj,
-                         @width,
-                         @add_introns,
-                         style_override),
-              :type => "image/png",
-              :disposition => "inline",
-              :filename => "#{@annotation.name}_#{@sequence_region.seq_id}_#@start_#@end.png"
+    filename = "#{@annotation.name}_" +
+               "#{@sequence_region.seq_id}_" +
+               "#{@start}_#{@end}.png"
+    send_data GTServer.img(filename), :type => "image/png", 
+              :disposition => "inline", :filename => filename
+  end
+  
+  def overview
+    filename = "#{@annotation.name}_" +
+               "#{@sequence_region.seq_id}_" +
+               "overview.png"
+    send_data GTServer.img(filename), :type => "image/png", 
+              :disposition => "inline", :filename => filename
   end
 
   ### actions with a template ###
-
+  
   #
   # The main page, with the sequence region selector
   #
@@ -59,82 +60,83 @@ class ViewerController < ApplicationController
 
 private
 
-  #
-  # - check that the parameters are valid:
-  #   - the annotation must be specified and be valid 
-  #   - the annotation must be own or public 
-  #   - the sequence region must be valid if specified
-  #
-  # - load the relevant data into the following instance variables
-  #   that are used by the view templates:
-  #   - @annotation
-  #   - @sequence_region
-  #   - @own (is the annotation one of the owns?)
-  #   - @sequence_regions (list of all sequence regions of the annotation)
-  #   - @width (width of the displayed image)
-  #   - @seq_begin (begin of the whole sequence region)
-  #   - @seq_end (end of the whole sequence region)
-  #   - @start (begin of the current view)
-  #   - @end (end of the current view)
-  #   - @annotation_ft_settings (DB ft settings for this annotation)
-  #   - @ft_settings
-  #   - @add_introns (boolean value; add introns?)
-  #  
-  # - save settings in the database for logged in users showing own annotations
-  #   (ft_settings, add_introns)
-  #
   def initialization
-    # an annotation must be specified in the parameters, otherwise 
-    # nothing can be visualized:
-    raise "No annotation specified!" unless params[:annotation]
-    # load the annotation, including its sequence regions
-    @annotation = Annotation.first(:include => :sequence_regions,
-                                   :conditions =>
-                                   {:name => params[:annotation],
-                                    :user_id => User.
-                                          find_by_username(params[:username])})
-    raise "This annotation is not available." unless @annotation
-    # check permission
+    @user = user_from_params
+    @annotation = annotation_from_params
+    @sequence_regions = @annotation.sequence_regions
     @own = (@annotation.user == @current_user)
     raise "Private annotations can be visualized only by their owners." unless
                                                      @own or @annotation.public
-    # shortcut for the sequence regions collection:
-    @sequence_regions = @annotation.sequence_regions
+    @sequence_region = sequence_region_from_params
+    @seq_begin = @sequence_region.seq_begin
+    @seq_end   = @sequence_region.seq_end
+    @start = viewport_start_from_params
+    @end   = viewport_end_from_params
+    @width = determine_width
+    @annotation_ft_settings = @annotation.feature_type_in_annotations
+    @ft_settings = determine_ft_settings
+    @add_introns = determine_add_introns_flag    
+  rescue => err
+    flash[:errors] = err.to_s
+    redirect_to(@current_user ? own_files_url : root_url)
+  end
+  
+  def user_from_params
+    username = params["username"]
+    user = User.find_by_username(username)
+    raise "No such user: #{username}" unless user
+    return user
+  end
+  
+  def annotation_from_params
+    raise "No annotation specified." unless params["annotation"]
+    conditions = {:name => params[:annotation], :user_id => @user.id}
+    annotation = Annotation.first(:include => :sequence_regions,
+                                  :conditions => conditions)
+    # load the annotation, including its sequence regions
+    raise "This annotation is not available." unless annotation
+    return annotation
+  end
+  
+  def sequence_region_from_params
     # the current sequence region
     # if none specified in the parameters, the first one is taken
-    @sequence_region = params[:seq_region] ?
+    sequence_region = params[:seq_region] ?
           @sequence_regions.find_by_seq_id(params[:seq_region]) :
           @sequence_regions.first 
-    # the following may happen only if the seq id is specified in the params:
+    # the following may happen if a false seq_id is encoded in the URL:
     raise "Sequence region not available for this annotation." unless
-                                                               @sequence_region
-    # shortcuts for the begin and end of the sequence region
-    @seq_begin = @sequence_region.seq_begin
-    @seq_end = @sequence_region.seq_end
-    # where should the current view start? Determine the start point, when 
-    # available using params[:start_point], and correct clear errors 
-    # (i.e. start point >= end of the sequence or < start of the sequence).
-    @start = 
-      case 
-      when !params[:start_pos] : @seq_begin
-      when params[:start_pos].to_i < @seq_begin : @seq_begin
-      when params[:start_pos].to_i >= @seq_end : @seq_end - 1
-      else
-        params[:start_pos].to_i
-      end
-    # the same for the current view endpoint:
-    @end = 
-      case 
-      when !params[:end_pos] : @seq_end
-      when params[:end_pos].to_i > @seq_end : @seq_end
-      when params[:end_pos].to_i < @seq_begin : @seq_begin + 1
-      else
-        params[:end_pos].to_i
-      end
-      
-    # get width from the params, the session, the current user style or 
-    # a default value if everything else fails
-    @width = 
+                                                               sequence_region
+    return sequence_region
+  end
+  
+  # where should the current view start? Determine the start point, when 
+  # available using params[:start_point], and correct clear errors 
+  # (i.e. start point >= end of the sequence or < start of the sequence).
+  def viewport_start_from_params
+    case 
+    when !params[:start_pos]                  : @seq_begin
+    when params[:start_pos].to_i < @seq_begin : @seq_begin
+    when params[:start_pos].to_i >= @seq_end  : @seq_end - 1
+    else
+      params[:start_pos].to_i
+    end
+  end
+  
+  def viewport_end_from_params
+    case 
+    when !params[:end_pos]                  : @seq_end
+    when params[:end_pos].to_i > @seq_end   : @seq_end
+    when params[:end_pos].to_i < @start     : @start + 1
+    else
+      params[:end_pos].to_i
+    end
+  end
+  
+  # get width from the params, the session, the current user style or 
+  # a default value if everything else fails
+  def determine_width
+    session[:width] = # save it also in the session for future reference
       case 
       when params[:width] : params[:width].to_i
       when session[:width] : session[:width]
@@ -142,125 +144,65 @@ private
       else
         900
       end
-    # save it in the session   
-    session[:width] = @width
-    
-    # shortcut for the feature type settings saved for this annotation:
-    @annotation_ft_settings = @annotation.feature_type_in_annotations
-    
-    # get the ft settings specified in the settings form
-    @ft_settings = {}
-    if params[:commit] ### settings form ###
-      params[:ft].each do |ft_name, setting|
-        @ft_settings[ft_name] = {}
-        ft_id = FeatureType.find_by_name(ft_name).id
-        unless setting[:show]
-          @ft_settings[ft_name][:show] = 0
-          @ft_settings[ft_name][:capt] = 0
-        else
-          # nil means infinite, show at any width
-          show = (Integer(setting[:max_show_width]) rescue nil)
-          @ft_settings[ft_name][:show] = show
-          unless setting[:capt]
-            @ft_settings[ft_name][:capt] = 0
-          else
-            # again: nil means infinite, show at any width
-            capt = (Integer(setting[:max_capt_show_width]) rescue nil)
-            @ft_settings[ft_name][:capt] = capt
-          end
-        end
-        if @own
-          # save settings in the DB
-          ft_in_a = @annotation_ft_settings.find_by_feature_type_id(ft_id)
-          ft_in_a.max_show_width      = @ft_settings[ft_name][:show]
-          ft_in_a.max_capt_show_width = @ft_settings[ft_name][:capt]
-          ft_in_a.save
-        end
-      end
-    elsif session[:ft_settings] and  ### session hash ###
-            session[:ft_settings].
-              fetch(@annotation.name,{}).
-                fetch(@sequence_region.seq_id, false)
-      @ft_settings =
-        session[:ft_settings][@annotation.name][@sequence_region.seq_id]
-    else ### annotation default options ###
-      @annotation_ft_settings.each do |setting|
-        @ft_settings[setting.feature_type.name] = {}
-        @ft_settings[setting.feature_type.name][:show] =
-                                                setting.max_show_width
-        @ft_settings[setting.feature_type.name][:capt] =
-                                           setting.max_capt_show_width
-      end
-    end
-    # save settings in the session hash
-    session[:ft_settings] ||= {}
-    session[:ft_settings][@annotation.name] ||= {}
-    session[:ft_settings][@annotation.name][@sequence_region.seq_id] =
-                                                             @ft_settings
+  end
+  
+  def determine_add_introns_flag
     # add introns mode on?
     if params[:add_introns]
-      @add_introns = (params[:add_introns]=="1")
-    elsif session[:add_introns] and session[:add_introns][@annotation.name]
-      @add_introns = session[:add_introns][@annotation.name]
+      add_introns = (params[:add_introns]=="1")
+    elsif session[:add_introns] and session[:add_introns].has_key?(@annotation.name)
+      add_introns = session[:add_introns][@annotation.name]
     else
-      @add_introns = @annotation.add_introns
+      add_introns = @annotation.add_introns
     end
     # save in the session the add intron setting
     session[:add_introns] ||= {}
-    session[:add_introns][@annotation.name] = @add_introns
+    session[:add_introns][@annotation.name] = add_introns
     # save in the db if own annotation
-    @annotation.update_attributes(:add_introns => @add_introns) if @own
-  rescue => err
-    flash[:errors] = err.to_s
-    redirect_to(@current_user ? own_files_url : root_url)
+    @annotation.update_attributes(:add_introns => add_introns) if @own
+    return add_introns
   end
   
   ### prepare_for_rendering ###
 
   def prepare_for_rendering
-    # overview
-    GTServer.img_and_map_generate(@annotation.gff3_data_storage, 
-                                  @sequence_region.seq_id, 
-                                  @seq_begin..@seq_end,
-                                  style_obj,
-                                  @width,
-                                  @add_introns,
-                                  style_override + overview_settings)
-    prepare_measures
-    # main image
-    GTServer.img_and_map_generate(@annotation.gff3_data_storage, 
-                                  @sequence_region.seq_id, 
-                                  @start..@end,
-                                  style_obj,
-                                  @width,
-                                  @add_introns,
-                                  style_override)
-    @info = GTServer.map(@annotation.gff3_data_storage, 
-                         @sequence_region.seq_id, 
-                         @start..@end,
-                         style_obj,
-                         @width,
-                         @add_introns,
-                         style_override)
-  end
-  
-  def overview_settings
     @overview_margins = 20
-    [
-      ['bool', 'format', 'show_track_captions', false],
-      ['bool', 'format', 'show_block_captions', false],
-      ['bool', 'format', 'split_lines',         false],
-      ['num',  'format', 'margins', @overview_margins],
-      ['num',  'format', 'bar_height',          2    ],
-      ['num',  'format', 'bar_vspace',          2    ],
-      ['num',  'format', 'track_vspace',        2    ],
-      ['num',  'format', 'scale_arrow_width',   3    ],
-      ['num',  'format', 'scale_arrow_height',  3    ],
-      ['num',  'format', 'arrow_width',         3    ]
-    ]
-  end
+    overview_settings = 
+      [
+        ['bool', 'format', 'show_track_captions', false],
+        ['bool', 'format', 'show_block_captions', false],
+        ['bool', 'format', 'split_lines',         false],
+        ['num',  'format', 'margins', @overview_margins],
+        ['num',  'format', 'bar_height',          2    ],
+        ['num',  'format', 'bar_vspace',          2    ],
+        ['num',  'format', 'track_vspace',        2    ],
+        ['num',  'format', 'scale_arrow_width',   3    ],
+        ['num',  'format', 'scale_arrow_height',  3    ],
+        ['num',  'format', 'arrow_width',         3    ]
+      ]
+      
+    GTServer.generate( "#{@annotation.name}_" +
+                      "#{@sequence_region.seq_id}_overview.png", 
+                      @annotation.gff3_data_storage, 
+                      @sequence_region.seq_id, 
+                      @seq_begin..@seq_end,
+                      style_obj,
+                      @width,
+                      @add_introns,
+                      overview_settings + style_override,
+                      false)
+    
+    @info = GTServer.generate("#{@annotation.name}_" +
+                      "#{@sequence_region.seq_id}_#{@start}_#{@end}.png",
+                      @annotation.gff3_data_storage, 
+                      @sequence_region.seq_id, 
+                      @start..@end,
+                      style_obj,
+                      @width,
+                      @add_introns,
+                      style_override, 
+                      true)
 
-  def prepare_measures
     @total_lenght   = (@seq_end - @seq_begin + 1).to_f
     @current_lenght = (@end - @start + 1).to_f
     overview_net_width = @width - (@overview_margins * 2)
@@ -270,7 +212,7 @@ private
                        overview_net_width).round + @overview_margins - 1
                        # -1 is to accomodate the slice div 1px border
   end
-
+  
   def style_obj
     @current_user ?
       # logged in: use the user style object
@@ -281,12 +223,65 @@ private
   end
     
   def style_override
-    output
+    output = []
     @ft_settings.each do |section, setting|
       output << ['num', section, 'max_show_width',      setting[:show]]
       output << ['num', section, 'max_capt_show_width', setting[:capt]]
     end
     return output
+  end
+
+
+  def determine_ft_settings
+    ft_settings = {}
+    if params[:commit] ### settings form ###
+      params[:ft].each do |ft_name, setting|
+        ft_settings[ft_name] = {}
+        if setting["show"] == "true"
+          # nil means infinite, show at any width
+          show = (Integer(setting["max_show_width"]) rescue nil)
+          ft_settings[ft_name][:show] = show
+          if setting["capt"] == "true"
+            # again: nil means infinite, show at any width
+            capt = (Integer(setting["max_capt_show_width"]) rescue nil)
+            ft_settings[ft_name][:capt] = capt
+          else
+            ft_settings[ft_name][:capt] = 0
+          end
+        else
+          ft_settings[ft_name][:show] = 0
+          ft_settings[ft_name][:capt] = 0
+        end
+        if @own
+          # save settings in the DB
+          ft_id = FeatureType.find_by_name_and_style_id(ft_name, @current_user.style.id).id
+          ft_in_a = @annotation_ft_settings.find_by_feature_type_id(ft_id)
+          ft_in_a.max_show_width      = ft_settings[ft_name][:show]
+          ft_in_a.max_capt_show_width = ft_settings[ft_name][:capt]
+          ft_in_a.save
+        end
+      end
+    elsif session[:ft_settings] and  ### session hash ###
+            session[:ft_settings].
+              fetch(@annotation.name,{}).
+                fetch(@sequence_region.seq_id, false)
+      ft_settings =
+        session[:ft_settings][@annotation.name][@sequence_region.seq_id]
+    else ### annotation default options ###
+      @annotation_ft_settings.each do |setting|
+        ft_settings[setting.feature_type.name] = {}
+        ft_settings[setting.feature_type.name][:show] =
+                                                setting.max_show_width
+        ft_settings[setting.feature_type.name][:capt] =
+                                           setting.max_capt_show_width
+      end
+    end
+    # save settings in the session hash
+    session[:ft_settings] ||= {}
+    session[:ft_settings][@annotation.name] ||= {}
+    session[:ft_settings][@annotation.name][@sequence_region.seq_id] =
+                                                             ft_settings
+    return ft_settings
   end
 
 end
--- test/functional/viewer_controller_test.rb
+++ test/functional/viewer_controller_test.rb
@@ -1,8 +1,191 @@
 require 'test_helper'
 
 class ViewerControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def setup
+    user_setup # see test_helper
   end
+
+  def test_username_from_params
+    get :index, :username => unknown_username
+    assert flash[:errors] =~ /user/i
+    get :index, {:username => @_u.username}
+    assert flash[:errors] !~ /user/i
+  end
+  
+  def test_annotation_from_params
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name}, 
+                {:user => @_u.id}
+    assert_equal @_a, assigns(:annotation) 
+  end
+  
+  def test_private_annotation_not_own
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name}
+    assert flash[:errors] =~ /private/i
+  end
+  
+  def test_sequence_region_from_params_implicit
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name}, 
+                {:user => @_u.id}
+    assert_equal @_sr, assigns(:sequence_region) 
+  end
+    
+  def test_sequence_region_from_params_explicit
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id}, 
+                {:user => @_u.id}
+    assert_equal @_sr, assigns(:sequence_region) 
+  end
+    
+  def test_sequence_region_from_params_error
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => "foobar"}, 
+                {:user => @_u.id}
+    assert flash[:errors] =~ /sequence region/i
+  end
+  
+  def test_viewport_start_from_params_implicit
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id}, 
+                {:user => @_u.id}
+    assert_equal 1000, assigns(:start)
+  end
+  
+  def test_viewport_start_from_params_explicit
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id,
+                 :start_pos => 2000}, 
+                {:user => @_u.id}
+    assert_equal 2000, assigns(:start)
+  end
+  
+  def test_viewport_start_from_params_too_low
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id, 
+                 :start_pos => 500}, 
+                {:user => @_u.id}
+    assert_equal 1000, assigns(:start)
+  end
+  
+  def test_viewport_start_from_params_too_high
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id, 
+                 :start_pos => 10000}, 
+                {:user => @_u.id}
+    assert_equal 8999, assigns(:start)
+  end
+  
+  def test_viewport_end_from_params_implicit
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id,
+                 :start_pos => 2000}, 
+                {:user => @_u.id}
+    assert_equal 9000, assigns(:end)
+  end
+  
+  def test_viewport_end_from_params_explicit
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id,
+                 :start_pos => 2000,
+                 :end_pos => 3000}, 
+                {:user => @_u.id}
+    assert_equal 3000, assigns(:end)
+  end
+  
+  def test_viewport_end_from_params_too_low
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id, 
+                 :start_pos => 2000,
+                 :end_pos => 1500}, 
+                {:user => @_u.id}
+    assert_equal 2001, assigns(:end)
+  end
+  
+  def test_viewport_end_from_params_too_high
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :seq_region => @_sr.seq_id, 
+                 :start_pos => 2000,
+                 :end_pos => 10000}, 
+                {:user => @_u.id}
+    assert_equal 9000, assigns(:end)
+  end
+  
+  def test_determine_width_from_params
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :width => 777}, 
+                {:user => @_u.id}
+    assert_equal 777, assigns(:width)
+    assert_equal 777, session[:width]
+  end
+  
+  def test_determine_width_from_session
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name}, 
+                {:user => @_u.id,
+                 :width => 770}
+    assert_equal 770, assigns(:width)
+    assert_equal 770, session[:width]
+  end
+
+  def test_determine_width_from_style
+    @_s.width = 700
+    @_s.save
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name}, 
+                {:user => @_u.id}
+    assert_equal 700, assigns(:width)
+    assert_equal 700, session[:width]
+  end
+
+  def test_determine_width_fallback
+    @_a.public = true
+    @_a.save
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name}
+    assert_equal 900, assigns(:width)
+    assert_equal 900, session[:width]
+  end
+
+  def test_determine_add_introns_from_params
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name,
+                 :add_introns => "0"}, 
+                {:user => @_u.id}
+    assert_equal false, assigns(:add_introns)
+    assert_equal false, session[:add_introns][@_a.name]
+  end
+  
+  def test_determine_add_introns_from_session
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name}, 
+                {:user => @_u.id,
+                :add_introns => {@_a.name => false}}
+    assert_equal false, assigns(:add_introns)
+    assert_equal false, session[:add_introns][@_a.name]
+  end
+  
+  def test_determine_add_introns_from_annotation
+    @_a.add_introns = false
+    @_a.save
+    get :index, {:username => @_u.username,
+                 :annotation => @_a.name}, 
+                {:user => @_u.id}
+    assert_equal false, assigns(:add_introns)
+    assert_equal false, session[:add_introns][@_a.name]
+  end
+  
 end
